51a49985adc1f69f1d172bbe185b7edc8817bbf3
haroldsr01 <advinculaharoldl.official@gmail.com>
2023-01-13 22:54:40 +0800

    add bootstrap react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import { Test } from './Test';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button>This is a button</Button>
+        <Test />
         <a
           className="App-link"
           href="https://reactjs.org"
